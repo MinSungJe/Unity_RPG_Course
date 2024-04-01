beb95b4758dc6888ce24381e586339cf8ab9bb93
MinSungJe <minje813@naver.com>
2024-04-01 15:56:54 +0900

    Endless background

--- Assets/ParallexBackground.cs
+++ Assets/ParallexBackground.cs
@@ -9,18 +9,26 @@ public class Pallalrex : MonoBehaviour
     [SerializeField] private float parallexEffect;
 
     private float xPosition;
+    private float length;
 
     void Start()
     {
         cam = GameObject.Find("Main Camera");
 
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
         xPosition = transform.position.x;
     }
 
     void Update()
     {
+        float distanceMoved = cam.transform.position.x * (1-parallexEffect);
         float distanceToMove = cam.transform.position.x * parallexEffect;
 
-        transform.position = new Vector3(xPosition + distanceToMove, transform.position.y);        
+        transform.position = new Vector3(xPosition + distanceToMove, transform.position.y);
+
+        if (distanceMoved > xPosition + length)
+            xPosition += length;
+        else if (distanceMoved < xPosition - length)
+            xPosition -= length;
     }
 }
