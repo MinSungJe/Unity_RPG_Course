e7636190580a325ca39de639f9b12c9009077daa
Minje Sung <minje813@dgu.ac.kr>
2024-04-27 20:14:18 +0900

    Setting up details of the sword

--- Assets/Scripts/Skills/Sword_Skill.cs
+++ Assets/Scripts/Skills/Sword_Skill.cs
@@ -8,4 +8,11 @@ public class Sword_Skill : Skill
     [SerializeField] private GameObject swordPrefab;
     [SerializeField] private Vector2 launchDir;
     [SerializeField] private float swordGravity;
+
+    public void CreateSword()
+    {
+        GameObject newSword = Instantiate(swordPrefab, player.transform.position, player.transform.rotation);
+
+        newSword.GetComponent<Sword_Skill_Controller>().SetupSword(launchDir, swordGravity);
+    }
 }
